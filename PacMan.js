79d13473e2310fb850dab38f3724a0ac63d9dfcf
T. E. Shaw <t.e.shaw@dahoum.wales>
2023-03-14 16:25:02 +0100

    homework

---
+++ PacMan.js
@@ -0,0 +1,13 @@
+// Here we read the keyboard
+document.addEventListener("keydown", (event) => {
+    alert(event.key);
+    // HOMEWORK
+    // Implement a very simple movement
+    // change pacman to id=
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById "pacman"
+    // this is an object and you can edit its postioin on the screen
+    // https://stackoverflow.com/questions/6802956/how-to-position-a-div-in-a-specific-coordinates
+    // find the current coordinates
+    // increase them w/ 1
+    // apply them
+});
--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 * Today
   * Begin to think how to move the PacMan /??
     * (We will animate with JavaScript)
+  * https://en.wikipedia.org/wiki/Semiotics
 * Mantra
   * Prepare to share screen and code while others are watching (one day) ;)
   * Keep and archive the diary.
   * Pull before work.
   * Make small steps.
   * https://dahoum.wales/the-pacman-as-a-nursery-32ba07b282b1
+* Homework
+  * Understand what we did today
+  * Implement a very simple movement inside the event function

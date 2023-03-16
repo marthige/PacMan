02166d5f9eee904c043d1c1d9988b426c4df7bae
marthige <marta.georgieva2004@gmail.com>
2023-03-16 18:16:32 +0200

    first idea for movement

--- PacMan.js
+++ PacMan.js
@@ -1,11 +1,31 @@
 // Here we read the keyboard
 document.addEventListener("keydown", (event) => {
-    alert(event.key);
-    
+    // alert(event.key);
+    var p = document.getElementById(pacman);
+    switch(p) {
+        case 37: 
+            p.style.position="absolute";
+            p.style.left= x_pos-1;
+            p.style.top= y_pos;
+            break;
+        case 38: 
+            p.style.position="absolute";
+            p.style.left= 30;
+            p.style.top= 156.5-'px';
+            break;
+        case 39: 
+            p.style.position="absolute";
+            p.style.left= 30+'px';
+            p.style.top= 156.5;
+            break;
+        case 40: 
+            p.style.position="absolute";
+            p.style.left= 30;
+            p.style.top= 156.5+'px';
+            break;
+    }
 
-    // HOMEWORK
-    // Implement a very simple movement
-    // change pacman to id=
+
     // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById "pacman"
     // this is an object and you can edit its postioin on the screen
     // https://stackoverflow.com/questions/6802956/how-to-position-a-div-in-a-specific-coordinates

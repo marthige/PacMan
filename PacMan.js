dd74705fbcc83b145796493c8bd7e23c08508f38
marthige <marta.georgieva2004@gmail.com>
2023-03-30 19:54:21 +0300

    30-03 update and working code for the pacman

--- PacMan.js
+++ PacMan.js
@@ -7,62 +7,24 @@ document.addEventListener("keydown", (event) => {
 
             var y = parseInt(getComputedStyle(pacman).top);
 
-            if (ev = 38)
+            if (ev == "ArrowUp")
                 --y;
-            else if (ev == 40)
+            else if (ev == "ArrowDown")
                 ++y;
             return y;
         },
-
+        
         x : () => {
             var x = parseInt(getComputedStyle(pacman).left);
 
-            if (ev == 37)
+            if (ev == "ArrowLeft")
                 --x;
-            else if (ev == 39)
+            else if (ev == "ArrowRight")
                 ++x;
             return x;
         }
     }
-
     pacman.style.top = (move.y()) + "px";
     pacman.style.left = (move.x()) + "px";
-    
 
-
-
-
-    //alert(event.key);  detects and shows that key what was clicked
-
-   /* switch(event.key) {
-        case 37: 
-            p.style.position = "absolute";
-            p.style.left = 30 - 1;
-            p.style.top = 156.5;
-            break;
-        case 38: 
-            p.style.position = "absolute";
-            p.style.left = 30;
-            p.style.top = 156.5 - 1;
-            break;
-        case 39: 
-            p.style.position = "absolute";
-            p.style.left = 30 + 1;
-            p.style.top = 156.5;
-            break;
-        case 40: 
-            p.style.position = "absolute";
-            p.style.left = 30;
-            p.style.top = 156.5 + 1;
-            break;
-    }*/
-    console.log(1); // checks what part works, even if it doesnt show
-
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById "pacman"
-    // this is an object and you can edit its postioin on the screen
-    // https://stackoverflow.com/questions/6802956/how-to-position-a-div-in-a-specific-coordinates
-    // find the current coordinates
-    // increase them w/ 1
-    // apply them
 });
